4a4b5c4a322ecfb327b83f8a9659d79648f320c7
Brandin Jefferson <brandinjefferson93@gmail.com>
2014-08-20 19:28:28 -0500

    Working on Density Grid
    Made some more progress and changed the Record class a lot. The
    dimensions are handled now.

--- src/DStreamTest.java
+++ src/DStreamTest.java
@@ -4,25 +4,165 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Scanner;
+import java.util.Set;
+
+import twitter4j.StallWarning;
 import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
 import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.AccessToken;
 
 public class DStreamTest {
 
 	//Object[][] density_grid = new Object[8][8];
-	public static int dimensions; //The number of unique words found
+	public static int dimensions = 0; //The number of unique words found
+	private static int timestamp; // t, the current time as an integer
+	private static int gaptime = 100; // gap, displays the total 
+	public static ArrayList<String> uniqueRecords; //I may have been using this incorrectly
+												//Maybe this should be covered by the density grid
+	public static Collection<Set<Set<Record>>> densitygrid; //Collection = S, First Set = Si/grids, Second Set = ji/partitions
+	public static RedBlackTree gridlist;
+	
+	
+	private static TwitterStream twitStream;
+	private static String oauthconsumerkey = "XJzkH9T32B631ylqiuA";
+	private static String oauthconsumersecret = "xpf1n34oCarn7sOYQXCPuMlAjKQtPtFaxeq2JnaxFg";
+	private static String accesstoken = "712592654-zzA9hbH5RXAb555ee46KvUYtPVZS1hV4BPHsf5Sq";
+	private static String accesstokensecret = "G0ZPtNF2DEpUgWHLEDMT8UYWGSs3KxDFlpdI0Uk";
 	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 		/*for (int i = 0;i<8;i++){
 			for (int j=0;j<8;j++){
 				density_grid[i][j] = new ArrayList<Record>();
 			}
 		}*/
-		
-		
+		Scanner reader = new Scanner(System.in);
+		System.out.print("Begin (0 = No, 1 = Yes): ");
+		int choice = reader.nextInt();
+		if (choice == 1){
+			getTweets();
+		}
+		reader.close();
 	}
 
+	private void getTweets(){
+		timestamp = 0;
+		gridlist = new RedBlackTree();
+		StatusListener listener = new StatusListener(){
+
+			@Override
+			public void onException(Exception arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onDeletionNotice(StatusDeletionNotice arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onScrubGeo(long arg0, long arg1) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStallWarning(StallWarning arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void onStatus(Status status) {
+				// TODO Auto-generated method stub
+				//Think about putting these in different threads
+				String[] tokens = tokenizeTweet(status);
+				addDimensions(tokens);			//Add total dimensions from this tweet
+				
+				ArrayList<Record> currentRecords = convertDataRecords(tokens);
+				updateConnections(currentRecords);
+				
+				
+				timestamp+=1;
+			}
+
+			@Override
+			public void onTrackLimitationNotice(int arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		twitStream = new TwitterStreamFactory().getInstance();
+		twitStream.addListener(listener);
+		twitStream.setOAuthConsumer(oauthconsumerkey, oauthconsumersecret);
+		twitter4j.auth.AccessToken accessToken = new AccessToken(accesstoken, accesstokensecret);
+		twitStream.setOAuthAccessToken(accessToken);
+		twitStream.sample(); //Getting random tweets right now
+	}
+	
+	private String[] tokenizeTweet(Status status){
+		String text = status.getText();
+		text = text.toLowerCase();
+		text = text.replaceAll("[^\\p{L}\\p{N}]"," ");
+		String[] temp = text.split(" ");
+		return temp;
+	}
+	
+	//Remember that if a record is already within the list, update its grid.
+	private ArrayList<Record> convertDataRecords(String[] tokens){
+		ArrayList<Record> tempArray = new ArrayList<Record>();
+		for (String text : tokens){
+			Record newRecord = new Record(text,timestamp);
+			newRecord.initConnections(dimensions);
+			if (!uniqueRecords.contains(text)){
+				newRecord.setAsNew(true);
+				uniqueRecords.add(text);
+			}
+			else{
+				newRecord.setAsNew(false);
+			}
+			tempArray.add(newRecord);
+		}
+		return tempArray;
+	}
+	
+	private void mapToGrid(Record rec){
+		
+	}
+	
+	//Should find which grid the record is in by comparing the words
+	private void findInGrid(String rec){
+		
+	}
+	
+	private void addDimensions(String[] tokens){
+		dimensions+=tokens.length;
+	}
+	
+	//Should be in its own thread? This could take a while
+	private void updateConnections(ArrayList<Record> tokens){
+		int ct = 0;
+		for (int i=0;i<tokens.size();i++){
+			for (int j=0;j<uniqueRecords.size();j++){
+				if (tokens.contains(new Record(uniqueRecords.get(j),timestamp))){
+					Record temp = tokens.get(i);
+					temp.addConnection(j);
+					ct+=1;
+				}
+				if (ct==tokens.size()){
+					ct=0;
+					break;
+				}
+			}
+		}
+	}
+	
 }
--- src/Record.java
+++ src/Record.java
@@ -2,16 +2,18 @@ import java.util.Collections;
 import java.util.ArrayList;
 import twitter4j.Status;
 
-public class Record {
+public class Record extends Object {
 	private String word;
 	private Double densityCoefficient;
 	private int timestamp;
-	public ArrayList<Record> connections;
+	public ArrayList<Integer> connections;	//Counts number of times word has shown up with the current record
+											//Use uniqueRecords as reference for indexes
+	private boolean newrecord;				//Set to true if this record is new, false if it has been seen before
 	
 	public Record(String _word, int time){
 		this.word = _word;
 		this.timestamp = time;
-		this.connections = new ArrayList<Record>();
+		this.connections = new ArrayList<Integer>();
 		this.densityCoefficient = 1.0;
 	}
 	
@@ -34,17 +36,51 @@ public class Record {
 		return this.word;
 	}
 	
-	public void addConnection(Record rec){
-		for (Record e : this.connections){
+	public void setAsNew(boolean choice){
+		this.newrecord = choice;
+	}
+	
+	public boolean isNewRecord(){
+		return this.newrecord;
+	}
+	
+	public void updateTimestamp(int newTime){
+		this.timestamp = newTime;
+	}
+	
+	public void initConnections(int dimensions){
+		for (int i=0;i<dimensions;i++){
+			if (i==dimensions-1){
+				this.connections.add(1);
+			}
+			else{
+				this.connections.add(0);
+			}
+		}
+	}
+	
+	public void addConnection(int index){
+		/*for (Record e : this.connections){
 			if (e.getWord() == rec.getWord()){
 				System.out.println(e.getWord() + " is already connected to " + rec.getWord());
 				return;
 			}
 		}
-		this.connections.add(rec);
-		
+		this.connections.add(rec);*/
+		int a = this.connections.get(index);
+		a+=1;
+		this.connections.remove(index);
+		this.connections.add(index, a);
 	}
 	
-	
+	@Override
+	public boolean equals(Object other){
+		if (other == this) return true;
+		
+		if (!(other instanceof Record)) return false;
+		
+		Record r = (Record) other;
+		return r.word.equals(this.word);
+	}
 	
 }
